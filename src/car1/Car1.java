d2cf401b730c44c41ef8b9935bf3f1bb98e6a58e
Student <Student@ZAPRE-D00000154.DirectDelivery.com>
2017-10-12 10:58:51 +0200

    initial project with another code added

--- src/car1/Car1.java
+++ src/car1/Car1.java
@@ -11,7 +11,9 @@ package car1;
  */
 public class Car1 {
 public int Add(String s1, String s2){
-return 0;
+   int d1 = Integer.parseInt(s1);
+   int d2 = Integer.parseInt(s2);
+return d1+d2;
 }
 
     public static void main(String[] args) {
