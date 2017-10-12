f502d6dcba698f2d9b977b4813e2757a6cc1a57a
Student <Student@ZAPRE-D00000154.DirectDelivery.com>
2017-10-12 10:53:07 +0200

    initial project

---
+++ src/car1/Car1.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package car1;
+
+/**
+ *
+ * @author Student
+ */
+public class Car1 {
+public int Add(String s1, String s2){
+return 0;
+}
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
